2d2628985438055f22c8921113029f5cd141819d
Samuel <a22samgarhur@inspedralbes.cat>
2023-09-27 08:08:26 +0200

    Cambios importantes y añadido JSON

--- app/src/main/java/com/example/projectepeliculesandroid/MainActivity.java
+++ app/src/main/java/com/example/projectepeliculesandroid/MainActivity.java
@@ -4,76 +4,137 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
-import android.widget.TextView;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    public int acertades=0;
+    public ArrayList respostes = new ArrayList();
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        RadioGroup grup = findViewById(R.id.grupo);
-        RadioButton correcto = findViewById(R.id.radioButton1);
-        RadioButton fals = findViewById(R.id.radioButton2);
+        try {
+            JSONObject obj = new JSONObject(loadJSONFromAsset());
+            JSONArray objecte = obj.getJSONArray("preguntes");
 
-        TextView resp = findViewById(R.id.respuesta);
+            for (int i =0;i<objecte.length();i++){
+                JSONObject preguntes = objecte.getJSONObject(i);
+                //Log.d("Data:",preguntes.toString()); //Para imprimir todo el contenido del JSON
 
+                String id = preguntes.getString("id");
+                String pregunta = preguntes.getString("pregunta");
+                Log.d("Numero de pregunta",id);
+                Log.d("Pregunta",pregunta);
 
+                JSONArray respuestas=preguntes.getJSONArray("respostes");
+                List<String> listarespuestas=new ArrayList<String>();
 
-        /*public void onRadioButtonClicked(View view) {
-            // Is the button now checked?
-            boolean checked = ((RadioButton) view).isChecked();
-            // Check which radio button was clicked.
-            switch (view.getId()) {
-                case R.id.radioButton1:
-                    if (checked)
-                        resp.setText("Respuesta correcta");
-                        resp.setTextColor(Color.GREEN);
-                        resp.setVisibility(View.VISIBLE);
-                    break;
-                case R.id.radioButton2,R.id.radioButton3,R.id.radioButton4:
-                    if (checked)
-                        resp.setText("Respuesta incorrecta");
-                        resp.setTextColor(Color.RED);
-                        resp.setVisibility(View.VISIBLE);
-                    break;
+                for (int j =0;j<respuestas.length();j++){
+                    JSONObject respuesta = respuestas.getJSONObject(j);
+                    listarespuestas.add(respuesta.getString("resposta"));
+                }
+                Log.d("Respostes",listarespuestas.toString());
 
-                    // Do nothing.
-                    break;
             }
-        }*/
+         } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
 
-        /*correcto.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (correcto.isChecked()) {
+        Button enviar = findViewById(R.id.butoEnviar);
+        enviar.setOnClickListener(this);
+         }
+
+    //Funcion para leer un JSON de la carpeta de assets
+    private String loadJSONFromAsset() {
+        String json;
+        try {
+            InputStream is = this.getAssets().open("Questionari JSON.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        Log.e("data", json);
+        return json;
+    }
+
+
+    public void onClick(View v) {
+            respostes=new ArrayList();
+            TextView respostafinal = findViewById(R.id.respostafinal);
+            RadioGroup grup = findViewById(R.id.grupo);
+            RadioGroup grup2 = findViewById(R.id.grupo2);
+
+            int id=grup.getCheckedRadioButtonId();
+            RadioButton radio = findViewById(id);
+
+            int id2=grup2.getCheckedRadioButtonId();
+            RadioButton radio2 = findViewById(id2);
+
+            if (respostafinal.getVisibility() == v.INVISIBLE) {
+                respostafinal.setVisibility(v.VISIBLE);
+                respostes.add(radio.getText());
+                respostes.add(radio2.getText());
+                respostafinal.setText("Respostes"+respostes);
+
+            } else {
+                respostafinal.setVisibility(v.INVISIBLE);
+                ;
+            }
+        }
+    public void onRadioButtonClicked(View view) {
+        TextView resp = findViewById(R.id.respuesta);
+        TextView contador = findViewById(R.id.contador);
+
+
+        contador.setText("Total acertades: "+acertades);
+
+        boolean checked = ((RadioButton) view).isChecked();
+
+        switch (view.getId()) {
+            case R.id.radioButton1:
+                if (checked)
                     resp.setText("Respuesta correcta");
                     resp.setTextColor(Color.GREEN);
                     resp.setVisibility(View.VISIBLE);
-                }
-
-
-            }
-
-        });
-        falso.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                if (falso.isChecked()) {
+                    acertades++;
+                    //RadioButton radioButton1=findViewById(R.id.radioButton1);
+                    //respostes.add(radioButton1.getText());
+                break;
+            case R.id.radioButton2:
+            case R.id.radioButton3:
+            case R.id.radioButton4:
+                if (checked)
                     resp.setText("Respuesta incorrecta");
                     resp.setTextColor(Color.RED);
                     resp.setVisibility(View.VISIBLE);
+                break;
 
-                }
-            }
-
-
-        });*/
+        }
     }
 
-}
+    }
+
